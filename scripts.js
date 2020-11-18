c7c26f819638d0ce22b0a3fb2c4f251dfc509953
AdeizaHub <32746634+AdeizaHub@users.noreply.github.com>
2020-11-18 19:48:06 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,43 @@
+// username && password validation
+function validate(event) {
+  event.preventDefault();
+
+  function show() {
+    var p = document.getElementById("password");
+    p.setAttribute("type", "text");
+  }
+
+  function hide() {
+    var p = document.getElementById("password");
+    p.setAttribute("type", "password");
+  }
+
+  var pwShown = 0;
+
+  document.getElementById("eye").addEventListener(
+    "click",
+    function () {
+      if (pwShown == 0) {
+        pwShown = 1;
+        show();
+      } else {
+        pwShown = 0;
+        hide();
+      }
+    },
+    false
+  );
+
+  var getUsername = document.querySelector("#Username").value;
+  var getPassword = document.querySelector("#Password").value;
+
+  if (getUsername === "") {
+    alert("Enter Username!!");
+    return;
+  }
+  if (getPassword === "") {
+    alert("Enter Password");
+    return;
+  }
+  alert("Login Successful!!");
+}
